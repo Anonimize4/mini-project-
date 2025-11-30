3a5c2ea864e7935ae065bd6734db6d8775deb342
Anonism123 <samuelbelay81@gmail.com>
2025-11-30 06:41:33 -0500

    navbar commited

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,15 @@ const Header: React.FC = () => {
     <header className="bg-blue-950 w-full h-16 flex items-center justify-between px-4 fixed top-0 left-0 right-0 z-50 shadow-lg">
      
       <div className="text-white text-xl font-bold">
-        samuel
+      SAMUEL BELAY
       </div>
       <nav className="flex space-x-4">
         <a href="#home" className="text-white hover:text-blue-300">Home</a>
         <a href="#about" className="text-white hover:text-blue-300">About</a>
         <a href="#skills" className="text-white hover:text-blue-300">Skills</a>
         <a href="#projects" className="text-white hover:text-blue-300">Projects</a>
+        <a href="#certificates" className="text-white hover:text-blue-300">Certificates</a>
+        <a href="#contact" className="text-white hover:text-blue-300">Contact</a>
       </nav>
     </header>
   );
