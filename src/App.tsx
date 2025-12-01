2a2a82fe590972b8b38427b718f0ff3b17a6fc04
Anonism123 <samuelbelay81@gmail.com>
2025-12-01 07:13:22 -0500

    color change

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import CyberBackground from './components/CyberBackground';
 
 function App() {
   return (
-      <div className="pt-16 min-h-screen bg-blue-950 flex flex-col relative">
+      <div className="pt-16 min-h-screen bg-cosmos flex flex-col relative">
       <CyberBackground />
       <div className="relative z-10">
         <Header />
